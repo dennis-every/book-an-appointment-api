5332fe2c9000b03a2556fa67d9e35ca119634eaf
Nkaleth <nseguralu@gmail.com>
2023-06-08 12:18:16 -0500

    ADD validations for all models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :bill, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
