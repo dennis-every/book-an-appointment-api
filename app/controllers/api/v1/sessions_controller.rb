971abdb7d62157e3a27f0b454cad4ac78141f92d
Dennis Every <dennis@ansyra.com>
2023-06-13 10:16:21 -0400

    Add login path, sessions#create

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        user = User.find_by(name: params[:name])
+        if user
+          render json: { message: 'Logged in successfully' }
+        else
+          render json: { error: 'Invalid user' }, status: :unauthorized
+        end
+      end
+    end
+  end
+end
