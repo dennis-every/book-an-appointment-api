5c7f97507ff282aef48af12931bf0894d2b9b8f3
dfsalam <dfsalam@gmail.com>
2023-06-13 14:33:14 -0500

    Add reservations as a nested route to users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :places
-      resources :reservations
-      resources :users
+      resources :places      
+      resources :users do
+        resources :reservations, only: [:index]
+      end
       post '/login', to: 'sessions#create'
     end
   end
