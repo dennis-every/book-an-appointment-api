b8bd2b319d7eca0ccaba65065e83b72adc674a84
Dennis Every <dennis@ansyra.com>
2023-06-16 12:21:53 -0400

    Add reservations#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :places
-      resources :reservations, only: [:create]
+      resources :reservations, only: [:create, :show]
       resources :users do
         resources :reservations, only: [:index]
       end
