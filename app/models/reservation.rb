cc7064c3dc4c19ac7a748c3ab94cf3e9240d5715
Dennis Every <dennis@ansyra.com>
2023-06-16 14:03:26 -0400

    Add before_validation set_bill

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,12 +2,18 @@ class Reservation < ApplicationRecord
   belongs_to :customer, class_name: 'User'
   belongs_to :place
 
+  before_validation :set_bill
+
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :bill, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validate :end_date_after_start_date
 
   private
+
+  def set_bill
+    self.bill = (end_date - start_date).to_i * place.rate
+  end
 
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
