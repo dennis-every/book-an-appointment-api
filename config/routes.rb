603d574cf33e89cc163e2cc184920c2ef3d08a01
Dennis Every <dennis@ansyra.com>
2023-06-16 11:51:41 -0400

    Update create reservations route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :places do
-        resources :reservations, only: [:create]
-      end     
+      resources :places
+      resources :reservations, only: [:create]
       resources :users do
         resources :reservations, only: [:index]
       end
