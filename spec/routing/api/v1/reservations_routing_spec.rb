6d293282292e304b27ab99b65e6bfbe7c08a5d8c
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-20 21:10:58 +0500

    Fix reservations route

--- spec/routing/api/v1/reservations_routing_spec.rb
+++ spec/routing/api/v1/reservations_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::ReservationsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/api/v1/reservations').to route_to('api/v1/reservations#index')
+      expect(get: '/api/v1/users/1/reservations').to route_to('api/v1/reservations#index', user_id: '1')
     end
 
     it 'routes to #show' do
